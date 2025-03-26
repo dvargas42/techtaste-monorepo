a661c54372f7aeed1e2f4b2c6f3d9ff9ac8c74b9
dvargas42 <dvargas.eng@gmail.com>
2025-03-26 00:10:18 -0300

    feat: :sparkles: implement circuitbreaker in OrderService

--- order-ms/src/main/java/com/techtaste/order_ms/service/OrderService.java
+++ order-ms/src/main/java/com/techtaste/order_ms/service/OrderService.java
@@ -7,9 +7,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.techtaste.order_ms.dto.AuthorizationDTO;
 import com.techtaste.order_ms.dto.OrderRequestDTO;
 import com.techtaste.order_ms.dto.OrderResponseDTO;
 import com.techtaste.order_ms.enums.Status;
+import com.techtaste.order_ms.http.AuthorizePaymentClient;
 import com.techtaste.order_ms.model.Order;
 import com.techtaste.order_ms.repository.OrderRepository;
 
@@ -17,17 +19,27 @@ import com.techtaste.order_ms.repository.OrderRepository;
 public class OrderService {
     
     private final OrderRepository repository;
+    private final AuthorizePaymentClient client;
 
-    public OrderService(OrderRepository repository) {
+    public OrderService(OrderRepository repository, AuthorizePaymentClient client) {
         this.repository = repository;
+        this.client = client;
     }
 
-    public OrderResponseDTO createOrder(OrderRequestDTO orderDTO) {
+    public OrderResponseDTO createOrder(OrderRequestDTO orderDTO, Boolean haveError) {
         Order order = new Order();
         BeanUtils.copyProperties(orderDTO, order);
-        order.setStatus(Status.WAITING_PAYMENT);
+        Status status = Status.WAITING_PAYMENT;
+        order.setStatus(status);
         order.setDate(LocalDate.now());
         order.calculateTotalValue();
+        repository.save(order);
+        if (haveError) {
+            status = Status.ERROR_CONSULTATION_PAYMENT;
+        } else {
+            status = getPaymentStatus(order.getId().toString());
+        }
+        order.setStatus(status);
         repository.save(order);
         return new OrderResponseDTO(
                 order.getId(), 
@@ -43,5 +55,13 @@ public class OrderService {
                 .map(order -> new OrderResponseDTO(order.getId(), order.getStatus(),
                         order.getCpf(), order.getItems(), order.getTotalValue(), order.getDate()))
                 .collect(Collectors.toList());            
+    }
+
+    private Status getPaymentStatus(String id) {
+        AuthorizationDTO authorization = client.getAuthorization(id);
+        if (authorization.status().equalsIgnoreCase("APPROVED")) {
+            return Status.PREPARING;
+        }
+        return Status.REFUSED;
     }
 }
