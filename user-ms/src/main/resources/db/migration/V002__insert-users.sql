a0701cf217aa140b73c2d2a1af1893ff68c38468
dvargas42 <dvargas.eng@gmail.com>
2025-03-28 18:08:45 -0300

    feat: :sparkles: change register name and email

--- user-ms/src/main/resources/db/migration/V002__insert-users.sql
+++ user-ms/src/main/resources/db/migration/V002__insert-users.sql
@@ -1,5 +1,5 @@
 insert into users (cpf, name, email)
-values ('123.456.789-00', 'João Silva', 'joao.silva@email.com'),
+values ('123.456.789-00', 'Daniel Vargas', 'dvargas.eng@gmail.com'),
        ('987.654.321-00', 'Maria Oliveira', 'maria.oliveira@email.com'),
        ('111.222.333-44', 'Carlos Pereira', 'carlos.pereira@email.com');
        
